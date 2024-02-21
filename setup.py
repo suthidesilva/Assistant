2aef125861f3794f11b77d0213020d0e563f9c88
Suthira de Silva <112716110+suthidesilva@users.noreply.github.com>
2024-02-20 23:58:14 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a test to see if I know what I am doing',
+    author='BrianMann',
+    license='MIT',
+)
